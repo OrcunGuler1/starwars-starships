7fc6ac019990ee2078bf2c79218724031ea9c4ec
aykut <aykut.alagoz17@gmail.com>
2022-10-11 00:12:22 +0300

    feat: people component initialized
    Created the people component and person card

--- src/pages/People/People.tsx
+++ src/pages/People/People.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { Suspense, useState } from 'react'
+import { Await, useLoaderData } from 'react-router-dom'
+import { Data, Person } from '../../types/types'
+import PersonCard from './components/PersonCard'
 
 const People = () => {
-  return <div>People</div>
+  const { results } = useLoaderData() as Data
+
+  const [sort, setSort] = useState<string>()
+
+  const handleSort = (data: Person[]) => {
+    return data.filter((person: any) => person.gender === sort)
+  }
+
+
+  return <div className="container mx-auto flex flex-col">
+     <div className="mb-8 flex w-full flex-row justify-center gap-8">
+        <div className="flex flex-row gap-3">
+          <select name="" id="" onChange={(e) => setSort(e.currentTarget.value)}>
+      <option value="">Select</option>
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+      <option value="n/a">Not Applicable</option>
+    </select>
+      </div>
+    </div>
+
+    <div className="grid w-full lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-5">
+    <Suspense fallback={<>Loading..</>}>
+          <Await
+            resolve={results}
+            children={(results: Person[]) => (
+              <>
+                {handleSort(results).map(person => (
+                  <div>
+                    <PersonCard key={person.name} {...person}  ></PersonCard>
+                  </div>
+              ))}
+              </>
+            )}
+            errorElement={<div>Something went wrong</div>}
+          />
+        </Suspense>      
+    </div>
+    
+  </div>
 }
 
 export default People
---
+++ src/pages/People/components/PersonCard.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+import type { Person } from '../../../types/types'
+
+const PersonCard = (person: Person) => {
+  const navigate = useNavigate()
+  return (
+    <div
+      className="relative flex flex-col rounded border border-slate-900 bg-slate-600 text-white before:absolute before:flex before:h-full before:w-full before:place-content-center before:items-center before:justify-center before:hover:cursor-pointer before:hover:bg-slate-700 before:hover:bg-opacity-80 before:hover:backdrop-blur-sm before:hover:transition-all before:hover:duration-300 before:hover:content-['Go_to_Details']"
+    >
+      <header className="my-3">
+        <h2 className="text-center">{person.name}</h2>
+      </header>
+      <div className="mb-2 flex flex-col items-center justify-center gap-3">
+        <p className="text-center">Hair Colour: {person.hair_color}</p>
+        <p className="text-center">Skin Colour: {person.skin_color}</p>
+        <p className="text-center">Height: {person.height}</p>
+        <p className="text-center">Eye Colour: {person.eye_color}</p>
+      </div>
+    </div>
+  )
+}
+
+export default PersonCard
