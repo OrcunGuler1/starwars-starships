d495421aa52655d22c61e6ff33388ee9bd183598
Orcun Guler <orcunguler381@gmail.com>
2022-10-11 18:01:02 +0300

    refactor sorting for people

--- src/pages/People/People.tsx
+++ src/pages/People/People.tsx
@@ -1,31 +1,37 @@
-import { Suspense, useState } from 'react'
+import { uniqBy } from 'lodash'
+import { Suspense, useMemo, useState } from 'react'
 import { Await, useLoaderData } from 'react-router-dom'
-import { Data, Person } from '../../types/types'
+import Select from '../../components/Select/Select'
+import { Data, Person, SelectOption } from '../../types/types'
 import PersonCard from './components/PersonCard'
 
 const People = () => {
   const { results } = useLoaderData() as Data
-
-  const [sort, setSort] = useState<string>('all')
-
+  const values = useMemo(
+    () =>
+      uniqBy(
+        (results as Person[]).map((person: Person) => ({
+          value: person.name,
+          label: person.gender,
+        })),
+        'label',
+      ),
+    [results],
+  )
+  const [options, setOptions] = useState<SelectOption[]>([])
   const handleSort = (data: Person[]) => {
-    if (sort === 'all') return data
-    return data.filter((person: any) => person.gender === sort)
+    return data.filter((person: any) =>
+      options.find(opt => person.gender === opt.label),
+    )
   }
-
   return (
     <div className="container mx-auto flex flex-col">
-      <div className="mb-8 flex w-full flex-row justify-center gap-8">
-        <div className="flex flex-row gap-3">
-          <select name="" id="" onChange={e => setSort(e.currentTarget.value)}>
-            <option value="">Select</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="n/a">Not Applicable</option>
-          </select>
-        </div>
-      </div>
-
+      <Select
+        multi
+        options={values}
+        onChange={opt => setOptions(opt)}
+        value={options}
+      />
       <div className="grid w-full gap-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<>Loading..</>}>
           <Await
--- src/types/types.ts
+++ src/types/types.ts
@@ -124,4 +124,38 @@ type Vehicle = {
   vehicle_class: string
 }
 
+type SelectOption = {
+  label: string
+  value: string | number
+}
+
+type MultipleSelectProps = {
+  multi: true
+  value: SelectOption[]
+  onChange: (value: SelectOption[]) => void
+}
+
+type SingleSelectProps = {
+  multi?: false
+  value?: SelectOption
+  onChange: (value: SelectOption | undefined) => void
+}
+
+type SelectProps = {
+  options: SelectOption[]
+} & (SingleSelectProps | MultipleSelectProps)
+
+type HandlerProps = {
+  e: KeyboardEvent
+  options: SelectOption[]
+  isOpen: boolean
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+  selectOption: (option: SelectOption) => void
+  highlightIndex: number
+  setHighlightIndex: React.Dispatch<React.SetStateAction<number>>
+}
+
+export type { SelectProps, SelectOption, HandlerProps }
+
+
 export type { Film, Person, Planet, Specie, Starship, Vehicle }
