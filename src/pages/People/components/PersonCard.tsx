92aedc099de93fdad25dd64e3978a98f82adca87
Orcun Guler <orcunguler381@gmail.com>
2022-10-17 22:27:09 +0300

    refactor:clean up code

--- src/pages/People/components/PersonCard.tsx
+++ src/pages/People/components/PersonCard.tsx
@@ -5,13 +5,11 @@ import type { Person } from '../../../types/types'
 
 const PersonCard: FC<Person> = (person: Person) => {
   const navigate = useNavigate()
-
+  const { id, resource } = getUrlPaths(person.url)
   return (
     <div
       className={`relative flex flex-col rounded border border-slate-900 bg-slate-600 text-white before:absolute before:flex before:h-full before:w-full before:place-content-center before:items-center before:justify-center before:hover:cursor-pointer before:hover:bg-slate-700 before:hover:bg-opacity-80 before:hover:backdrop-blur-sm before:hover:transition-all before:hover:duration-300 before:hover:content-['Go_to_details']`}
-      onClick={() =>
-        navigate(`/people/${getUrlPaths(person.url as string).id}`)
-      }
+      onClick={() => navigate(`/${resource}/${id}`)}
     >
       <header className="my-3">
         <h2 className="text-center">{person.name}</h2>
--- src/pages/Planets/components/PlanetCard.tsx
+++ src/pages/Planets/components/PlanetCard.tsx
@@ -5,11 +5,11 @@ import { Planet } from '../../../types/types'
 
 const PlanetCard: FC<Planet> = planet => {
   const navigate = useNavigate()
-
+  const { id, resource } = getUrlPaths(planet.url)
   return (
     <div
       className="relative flex flex-col rounded border border-slate-900 bg-slate-600 text-white before:absolute before:flex before:h-full before:w-full before:place-content-center before:items-center before:justify-center before:hover:cursor-pointer before:hover:bg-slate-700 before:hover:bg-opacity-80 before:hover:backdrop-blur-sm before:hover:transition-all before:hover:duration-300 before:hover:content-['Go_to_Details']"
-      onClick={() => navigate(`/planets/${getUrlPaths(planet.url).id}`)}
+      onClick={() => navigate(`/${resource}/${id}`)}
     >
       <header className="my-3">
         <h2 className="text-center">{planet.name}</h2>
